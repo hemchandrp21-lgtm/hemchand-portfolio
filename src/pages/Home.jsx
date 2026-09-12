0648453eba53ea5766f7564aa5b1798649531d0e
Hemchand <hemchand@example.com>
2026-09-12 23:56:43 +0530

    Remove all elements except the Hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,13 @@
-import FilmOverlay from '../components/FilmOverlay';
-import CustomCursor from '../components/CustomCursor';
-import Header from '../components/Header';
 import Hero from '../components/Hero';
 
 function Home() {
   return (
-    <div className="min-h-screen bg-[#070707] text-white selection:bg-amber-400 selection:text-black font-sans">
-      <FilmOverlay />
-      <CustomCursor />
-      <Header />
-      <main>
-        <Hero />
-      </main>
+    <div className="min-h-screen bg-[#050505] text-white overflow-hidden">
+      <Hero />
     </div>
   );
 }
 
 export default Home;
+
 
