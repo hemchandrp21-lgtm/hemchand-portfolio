f88e2564fb3f811d890d357fe3a3192648e31d48
Hemchand <hemchand@example.com>
2026-09-12 23:55:07 +0530

    Keep only Hero section on home page, remove all other sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,6 @@ import FilmOverlay from '../components/FilmOverlay';
 import CustomCursor from '../components/CustomCursor';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
-import SelectedWork from '../components/SelectedWork';
-import AboutSection from '../components/AboutSection';
-import ExperienceSection from '../components/ExperienceSection';
-import ProcessSection from '../components/ProcessSection';
-import AiDesignSection from '../components/AiDesignSection';
-import SkillsSection from '../components/SkillsSection';
-import ContactSection from '../components/ContactSection';
-import Footer from '../components/Footer';
 
 function Home() {
   return (
@@ -18,25 +10,11 @@ function Home() {
       <CustomCursor />
       <Header />
       <main>
-        {/* 1. WHO I AM */}
         <Hero />
-        {/* 2. WHAT I DESIGN & WHAT I'VE WORKED ON */}
-        <SelectedWork />
-        {/* 3. HOW I THINK */}
-        <AboutSection />
-        {/* 5. WHERE I'VE WORKED */}
-        <ExperienceSection />
-        {/* 3. DESIGN PROCESS METHODOLOGY */}
-        <ProcessSection />
-        {/* 6. WHAT I'M LEARNING & AI PHILOSOPHY */}
-        <AiDesignSection />
-        <SkillsSection />
-        {/* 7. HOW TO CONTACT ME */}
-        <ContactSection />
       </main>
-      <Footer />
     </div>
   );
 }
 
 export default Home;
+
